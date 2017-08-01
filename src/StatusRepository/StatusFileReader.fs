79899ca948d48bafc3b648b36eca119b4c38eeb6
Anders Kofoed <anders.kfd@gmail.com>
2017-08-01 22:00:41 +0200

    Minor refactoring of statusRepo

--- src/StatusRepository/StatusFileReader.fs
+++ src/StatusRepository/StatusFileReader.fs
@@ -5,10 +5,13 @@ open System.IO
     
 type StatusUpdate = CsvProvider<"statusUpdate.csv">
 
+let parseTransactionFile = 
+    (Path.GetFullPath >> StatusUpdate.Load >> (fun (s:StatusUpdate) -> s.Rows |> Seq.toList))
+
 let loadAllStatusUpdates fileDirectory = 
     match Directory.GetFiles(fileDirectory, "*.csv") with
     | [||] -> [||]
-    | arr -> Array.map (Path.GetFullPath >> StatusUpdate.Load) arr
-    |> Array.map (fun (s:StatusUpdate) -> s.Rows |> Seq.toList) |> List.concat
+    | files -> files |> Array.map parseTransactionFile
+    |> List.concat
 
 
